055dc872c54c86bd672aad9302574fbf8b90d8a0
ysbaekFox <ysbaekFox@gmail.com>
2022-01-22 13:58:46 +0900

    BOJ 2644
    촌수 계산

--- DFS/BOJ_2644/boj_2644.cpp
+++ DFS/BOJ_2644/boj_2644.cpp
@@ -1,12 +1,46 @@
 #include <stdio.h>
 #include <vector>
 
+bool isMatched = false;
+
+std::vector<std::vector<int>> family;
+bool isVisited[101];
+
+int n, m;
+int count = 0;
+void dfs(int p1, int p2)
+{
+	count++;
+
+	isVisited[p1] = true;
+
+	for (int i = 0 ; i < family[p1].size(); i++)
+	{
+		if (family[p1][i] == p2)
+		{
+			isMatched = true;
+			return;
+		}
+
+		if (false == isVisited[family[p1][i]])
+		{
+			dfs(family[p1][i], p2);
+		}
+
+		if (true == isMatched)
+		{
+			return;
+		}
+	}
+
+	count--;
+}
+
+
 int main()
 {
 	int n = 0;
 	scanf("%d", &n);
-
-	std::vector<std::vector<int>> family;
 	family.resize(1 + n);
 
 	int p1 = 0, p2 = 0;
@@ -20,7 +54,12 @@ int main()
 		scanf("%d %d", &p, &c);
 
 		family[p].push_back(c);
+		family[c].push_back(p);
 	}
+
+	dfs(p1, p2);
+
+	isMatched ? printf("%d", count) : printf("-1");
 
 	return 0;
 }
