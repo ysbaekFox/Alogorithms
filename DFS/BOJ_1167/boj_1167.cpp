11b4dddfd786b6bf83ad957f3a34a5004ecef5c8
ysbaekFox <ysbaekFox@gmail.com>
2022-01-24 23:13:54 +0900

    BOJ 1167
    트리의 지름

--- DFS/BOJ_1167/boj_1167.cpp
+++ DFS/BOJ_1167/boj_1167.cpp
@@ -3,42 +3,38 @@
 #include <functional>
 #include <vector>
 
-std::vector<std::vector<int>> tree;
+//std::vector<std::vector<int>> tree;
+std::vector<std::pair<int, int>> tree[100001];
 
 int v;
 int n, m, dist;
 int totaldist = 0;
 
-bool visited[1000001];
+bool visited[100001];
 
 #define INF 100001
 
-int dfs(int start)
-{
-	visited[start] = true;
-
-	int dist = 0;
-	const int size = tree[start].size() - 1;
-	for (int i = 1; i <= size; i++)
-	{
-		if ( ( tree[start][i]  < INF ) && ( false == visited[i] ) )
-		{
-			dist = std::max(dist, dfs(i) + tree[start][i]);
-		}
-	}
-
-	return dist;
-}
+//int dfs(int start)
+//{
+//	visited[start] = true;
+//
+//	int dist = 0;
+//	const int size = tree[start].size() - 1;
+//
+//	for (int i = 1; i <= size; i++)
+//	{
+//		if ( ( tree[start][i]  < INF ) && ( false == visited[i] ) )
+//		{
+//			dist = std::max(dist, dfs(i) + tree[start][i]);
+//		}
+//	}
+//
+//	return dist;
+//}
 
 int main()
 {
 	scanf("%d", &v);
-	tree.resize(v + 1);
-	for (int i = 1; i <= v; i++)
-	{
-		tree[i].resize(v + 1);
-		std::fill(tree[i].begin(), tree[i].end(), INF);
-	}
 
 	for (int i = 1; i <= v; i++)
 	{
@@ -57,11 +53,11 @@ int main()
 				scanf("%d", &dist);
 			}
 
-			tree[n][m] = dist;
+			tree[n].push_back(std::make_pair(m, dist));
 		}
 	}
 
-	printf("%d", dfs(1));
+	//printf("%d", dfs(1));
 
 	return 0;
 }
